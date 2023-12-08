e6cdd842d4a928fcd3d73dda04203903cb302f18
Rgofman <116192052+Rgofman@users.noreply.github.com>
2023-12-08 12:01:07 -0500

    Update RouteGuard.js

--- components/RouteGuard.js
+++ components/RouteGuard.js
@@ -10,16 +10,15 @@ import { useState, useEffect } from 'react';
 const PUBLIC_PATHS = ['/login', '/register', '/', '/_error'];
 
 export default function RouteGuard(props) {
-    
+    const [searchHistory, setSearchHistory] = useAtom(searchHistoryAtom);
+    const [favouritesList, setFavouritesList] = useAtom(favouritesAtom);
+    const [authorized, setAuthorized] = useState(false);
 
     
 
     const router = useRouter();
 
     useEffect(() => {
-        const [searchHistory, setSearchHistory] = useAtom(searchHistoryAtom);
-        const [favouritesList, setFavouritesList] = useAtom(favouritesAtom);
-        const [authorized, setAuthorized] = useState(false);
         async function updateAtoms() {
             setFavouritesList(await getFavourites());
             setSearchHistory(await getHistory());
